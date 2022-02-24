21d5fb92fbbb509d0545600a2191de038e63ca58
lentiummmx <lentium.mmx@gmail.com>
2022-02-23 22:02:31 -0600

    solution_load_nd_display_imgs_ft_intrnt File(s) committed to adding download call to get the image and display in the app (v0.1.0)

---
+++ app/src/main/java/com/example/android/marsphotos/ui/adapters/BindingAdapters.kt
@@ -0,0 +1,37 @@
+package com.example.android.marsphotos.ui.adapters
+
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import coil.load
+import com.example.android.marsphotos.R
+
+/**
+ * Binding Adapters are annotated methods used to create custom setters for custom properties of your view.
+ * <code>
+    <ImageView
+    android:layout_width="wrap_content"
+    android:layout_height="wrap_content"
+    app:imageUrl="@{product.imageUrl}"/>
+ * </code>
+
+ * Example of a Binding Adapter:
+ * <code>
+    @BindingAdapter("imageUrl")
+    fun bindImage(imgView: ImageView, imgUrl: String?) {
+        imgUrl?.let {
+            // Load the image in the background using Coil.
+        }
+    }
+ * </code>
+*/
+@BindingAdapter("imageUrl")
+fun bindImage(imageView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = it.toUri().buildUpon().scheme("https").build()
+        imageView.load(imgUri) {
+            placeholder(R.drawable.loading_animation)
+            error(R.drawable.ic_broken_image)
+        }
+    }
+}
